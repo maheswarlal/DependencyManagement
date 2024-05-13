a81e8f1773c108c651bb0f91ff8cea59705c9c4f
Maheswar Lal <maheswar.lal@cellpointmobile.com>
2024-05-13 08:49:20 +0530

    error

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/maheswarlal/dog"
+	"github.com/maheswarlal/puppy"
+)
+
+func main() {
+	puppy.Taglal4()
+	puppy.Taglal5()
+	puppy.Taglal8()
+	puppy.Fromlal1()
+	puppy.Fromlal2()
+	S1 := puppy.Bark()
+	S2 := puppy.Barks()
+	fmt.Println(S1)
+	fmt.Println(S2)
+	S3 := puppy.BigBark()
+	fmt.Println(S3)
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.Bark())
+	fmt.Println(dog.WhenGrownUp("Tom"))
+
+}
